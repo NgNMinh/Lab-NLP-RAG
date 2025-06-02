c95e1dfe5077ce58390af5adc20712944959b356
Nguyen Ngoc Minh <nnminh433@gmail.com>
2025-06-03 02:00:01 +0700

    Update chatbot admin

--- agent.py
+++ agent.py
@@ -1,14 +1,22 @@
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
 from config import llm
-from tools import retrieve, search_paper
+from tools import retrieve, search_paper, get_all_personnel, get_all_intern, delete_user
+from langgraph.checkpoint.mongodb import MongoDBSaver
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
 
-memory = MemorySaver()
+load_dotenv()
 
-agent_executor = create_react_agent(
+# DB_URI = os.getenv("MONGO_URI")
+# checkpointer = MongoDBSaver.from_conn_string(DB_URI)
+checkpointer = MemorySaver()
+
+agent_user = create_react_agent(
     llm, 
     [retrieve, search_paper], 
-    checkpointer=memory, 
+    checkpointer=checkpointer, 
     prompt="""
 Bạn là trợ lí AI thân thiện chuyên hỗ trợ hỏi đáp cho NLP & KD Lab thuộc trường Đại học Tôn Đức Thắng (TDTU). 
 Khi bạn sử dụng bất kỳ tool nào (như 'retrieve' hoặc 'search_paper'), 
@@ -22,3 +30,12 @@ Ví dụ:
 Luôn đảm bảo người dùng nhìn thấy kết quả có ích từ tool.
 """
 ) 
+
+agent_admin = create_react_agent(
+    llm, 
+    [get_all_intern, get_all_personnel, retrieve, search_paper, delete_user], 
+    checkpointer=checkpointer, 
+    prompt="""
+Bạn là trợ lí AI thân thiện chuyên hỗ trợ quản trị viên của NLP & KD Lab thuộc trường Đại học Tôn Đức Thắng (TDTU).
+"""
+)
